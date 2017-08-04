d9983d520b897de7ec817c396a2182cf0fed91b2
Todoroki <todoroki.auone@gmail.com>
2017-08-05 02:57:13 +0900

    add nostrum

---
+++ config/_sample.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+# Discord Bot Settings
+config :nostrum,
+  token: "", # The token of your bot as a string
+  num_shards: :auto # The number of shards you want to run your bot under, or :auto.

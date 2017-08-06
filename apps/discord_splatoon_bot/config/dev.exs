b156c24a34709d34576ae3fb076dee6eaa2391f2
Todoroki / U-dory <todoroki.auone+github@gmail.com>
2017-08-06 12:45:22 +0900

    Delete dev.exs
    .gitignore修正に伴い

--- apps/discord_splatoon_bot/config/dev.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-# Discord Bot Settings
-config :nostrum,
-  token: "MzQwOTMyNzY0OTUyMDM1MzMw.DF7d_w.p0vaBIXTUB85u8OYLtmUeXzGUko", # The token of your bot as a string
-  num_shards: :auto, # The number of shards you want to run your bot under, or :auto.
-  bot_id: 340932764952035330

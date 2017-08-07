5231c6c732e855e97345631ae6eb99c7f4911706
Todoroki <todoroki.auone@gmail.com>
2017-08-08 00:55:04 +0900

    update _sample.exs

--- apps/discord_splatoon_bot/config/_sample.exs
+++ apps/discord_splatoon_bot/config/_sample.exs
@@ -3,5 +3,5 @@ use Mix.Config
 # Discord Bot Settings
 config :nostrum,
   token: "", # The token of your bot as a string
-  num_shards: :auto # The number of shards you want to run your bot under, or :auto.
+  num_shards: :auto, # The number of shards you want to run your bot under, or :auto.
   bot_id: 0

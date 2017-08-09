38954259e714da0f1775178965e93f57869b75bd
Todoroki <todoroki.auone@gmail.com>
2017-08-09 18:37:20 +0900

    ユーザーのボイスチャンネル取得を関数にまとめた

---
+++ apps/discord_splatoon_bot/lib/discord_splatoon_bot/util.ex
@@ -0,0 +1,23 @@
+defmodule DiscordSplatoonBot.Util do
+  @doc """
+  Returns the current voice channel of user, in the given server.
+  Returns `:error` if one is not in a voice channel.
+
+  ## Examples
+
+      iex> DiscordSplatoonBot.Util.get_voice_channel_id(server, message.author.id)
+      {:ok, 12346789}
+
+  """
+  @spec get_voice_channel_id(Nostrum.Struct.Guild.t, integer) :: {:ok, integer} | :error
+  def get_voice_channel_id(server = %Nostrum.Struct.Guild{}, user_id) do
+    server.voice_states
+    |> Enum.find(%{}, fn(map) -> map.user_id == user_id end)
+    |> Map.fetch(:channel_id)
+    |> IO.inspect
+    |> (fn
+      {:ok, nil} -> :error
+      other -> other
+    end).()
+  end
+end

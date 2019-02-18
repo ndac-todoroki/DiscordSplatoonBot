cdf6783d5a899db053ec7d4d5aca24937476322b
Ryo Konishi <todoroki.auone@gmail.com>
2019-02-18 21:42:19 +0900

    cleaner debugging

--- apps/discord_splatoon_bot/lib/discord_splatoon_bot/consumer.ex
+++ apps/discord_splatoon_bot/lib/discord_splatoon_bot/consumer.ex
@@ -1,18 +1,21 @@
 defmodule DiscordSplatoonBot.Consumer do
-  use Nostrum.TaskedConsumer
+  use Nostrum.Consumer
+  import Logger
 
   alias DiscordSplatoonBot.Command
 
   def start_link do
-    TaskedConsumer.start_link(__MODULE__)
+    Consumer.start_link(__MODULE__)
   end
 
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
-    IO.inspect(msg)
+    Logger.debug(inspect(msg))
     Command.handle(msg)
   end
 
-  def handle_event(any) do
+  # Default event handler, if you don't include this, your consumer WILL crash if
+  # you don't have a method definition for each event type.
+  def handle_event(_event) do
     :noop
   end
 end

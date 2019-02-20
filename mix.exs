5a0a2376119bf95b78ac98b872a7018c607504f1
Ryo Konishi <todoroki.auone@gmail.com>
2019-02-20 11:43:53 +0900

    Release at GCP App Engine

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DiscordSplatoonBot.Umbrella.Mixfile do
   def project do
     [
       apps_path: "apps",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -22,6 +22,8 @@ defmodule DiscordSplatoonBot.Umbrella.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:distillery, "~> 2.0"}
+    ]
   end
 end

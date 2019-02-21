847588ae90c637406d0b69ca513635ac49dd84b8
Ryo Konishi <todoroki.auone@gmail.com>
2019-02-21 16:44:14 +0900

    move some settings to umbrella global

--- config/dev.exs
+++ config/dev.exs
@@ -3,3 +3,14 @@ use Mix.Config
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+# Goth is used inside of Kane, and needs to read Google credentials
+config :goth,
+  json: "google_json_creds/discord_splatoon_bot.json" |> Path.expand(__DIR__) |> File.read!()
+
+# Discord Bot Settings
+config :nostrum,
+  # The token of your bot as a string
+  token: "MzQzNTg3OTExNTQwNjcwNDc0.D0wNSQ.bveLr_nVBkn2kk-3ua-wHEZLH44",
+  # The number of shards you want to run your bot under, or :auto.
+  num_shards: :auto,
+  bot_id: 0
--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,15 @@ use Mix.Config
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Goth is used inside of Kane, and needs to read Google credentials
+config :goth,
+  json: "google_json_creds/discord_splatoon_bot.json" |> Path.expand(__DIR__) |> File.read!()
+
+# Discord Bot Settings
+config :nostrum,
+  # The token of your bot as a string
+  token: "MzQzNTg3OTExNTQwNjcwNDc0.D0wNSQ.bveLr_nVBkn2kk-3ua-wHEZLH44",
+  # The number of shards you want to run your bot under, or :auto.
+  num_shards: :auto,
+  bot_id: 0

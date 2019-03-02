7868bc814559ddb6febf9797db2f74c63d320736
Ryo Konishi <todoroki.auone@gmail.com>
2019-03-02 15:26:13 +0900

    simplify codebase

--- apps/discord_splatoon_bot/lib/discord_splatoon_bot/command.ex
+++ apps/discord_splatoon_bot/lib/discord_splatoon_bot/command.ex
@@ -20,24 +20,16 @@ defmodule DiscordSplatoonBot.Command do
   @doc """
   "？ブキランダム" の体を取るものはヘルプ表示
   """
-  def execute(["？" <> method], msg) do
-    Util.help(msg, method)
-  end
-
-  def execute(["？" <> method | args], msg) do
-    Util.help(msg, method, args)
-  end
+  def execute(["？" <> method], msg), do: Util.help(msg, method)
+  def execute(["？" <> method | args], msg), do: Util.help(msg, method, args)
 
   def execute([@reply_prefix, "help"], msg), do: Util.help(msg, "help")
   def execute([@reply_prefix | []], msg), do: Util.help(msg, "ヘルプ")
 
-  def execute([@reply_prefix, "ping"], msg) do
-    Util.ping(msg)
-  end
+  def execute([@reply_prefix, "ping"], msg), do: Util.ping(msg)
 
-  def execute(["ブキランダム" | options], msg) do
-    Weapons.random_all(msg.channel_id, msg.author.id, options)
-  end
+  def execute(["ブキランダム" | options], msg),
+    do: Weapons.random_all(msg.channel_id, msg.author.id, options)
 
   def execute(["ブキラン１" | options], msg) do
     if msg.guild_id,
@@ -45,43 +37,26 @@ defmodule DiscordSplatoonBot.Command do
       else: Weapons.dm_random_one(msg.channel_id, options)
   end
 
-  def execute(["メンバーランダム" | options], msg) do
-    Guild.random_members(msg.channel_id, msg.author.id, options)
-  end
+  def execute(["メンバーランダム" | options], msg),
+    do: Guild.random_members(msg.channel_id, msg.author.id, options)
 
-  def execute(["ハイカラニュース" | options], msg) do
-    News.latest(msg.channel_id, options)
-  end
+  def execute(["ハイカラニュース" | options], msg), do: News.latest(msg.channel_id, options)
 
-  def execute(["ナワバリ情報" | options], msg) do
-    News.regular(msg.channel_id, options)
-  end
+  def execute(["ナワバリ情報" | options], msg), do: News.regular(msg.channel_id, options)
 
-  def execute(["ガチマ情報" | options], msg) do
-    News.gachi(msg.channel_id, options)
-  end
+  def execute(["ガチマ情報" | options], msg), do: News.gachi(msg.channel_id, options)
 
-  def execute(["リグマ情報" | options], msg) do
-    News.league(msg.channel_id, options)
-  end
+  def execute(["リグマ情報" | options], msg), do: News.league(msg.channel_id, options)
 
-  def execute(["バイト情報" | options], msg) do
-    News.salmon(msg.channel_id, options)
-  end
+  def execute(["バイト情報" | options], msg), do: News.salmon(msg.channel_id, options)
 
-  def execute(["スケジュール", "作成", "ハイカラニュース" | options], msg) do
-    Schedule.latest_news(msg.channel_id, msg.id, msg.guild_id, options)
-  end
+  def execute(["スケジュール", "作成", "ハイカラニュース" | options], msg),
+    do: Schedule.latest_news(msg.channel_id, msg.id, msg.guild_id, options)
 
-  def execute(["スケジュール", "作成", _other | options], msg) do
-    Schedule.not_yet_error(msg.channel_id)
-  end
+  def execute(["スケジュール", "作成", _other | options], msg), do: Schedule.not_yet_error(msg.channel_id)
 
-  def execute(["スケジュール", "削除", id | _], msg) do
-    Schedule.delete(msg.channel_id, id, msg.guild_id)
-  end
+  def execute(["スケジュール", "削除", id | _], msg),
+    do: Schedule.delete(msg.channel_id, id, msg.guild_id)
 
-  def execute(_, _) do
-    :noop
-  end
+  def execute(_, _), do: :noop
 end
